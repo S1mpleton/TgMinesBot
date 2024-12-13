805210638bf1d89a24657537a69c94f096d9e3ba
Tim <timofei2500@bk.ru>
2024-12-13 13:01:15 +0700

    basic version

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7604671876:AAHYAI24duDk94RXRATmGTJj30pI8jtePlY"  # токен бота
+CHANNEL_URL = "https://t.me/luckujime"  # Ссылка на канал
+CHANNEL_ID = "-1002148557340"  # id канала. Начинаеться с минуса
+ADMIN_ID = 7114573951 #tg id твой
+
+REF_URL = "https://1wzvro.top/casino/list?open=register&p=f7eo"  # Ссылка на регистрацию
